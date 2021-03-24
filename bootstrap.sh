dde89cb49305770a80313a882fe23317f975a40d
Jesse B. Hannah <jesse@jbhannah.net>
2021-03-24 12:44:57 -0700

    Add progress messages to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,24 @@
 
 set -euo pipefail
 
+export PIPENV_VERBOSITY=-1
+
+echo "### Creating virtual environment…\c"
 python3 -m venv --system-site-packages .venv
+echo "done! Entering virtual environment."
 . .venv/bin/activate
+
+echo "\n### Installing Pipenv"
 pip install pipenv pip-autoremove
+
+echo "\n### Installing runtime dependencies"
 pipenv install
+
+echo "\n### Bootstrapping Dotfiles"
 pipenv run ansible-playbook ansible/dotfiles.yml
+
+echo "\n### Cleaning up"
 pip-autoremove pipenv pip-autoremove -y
+echo "\n### Exiting virtual environment…\c"
 deactivate
+echo "done!"
