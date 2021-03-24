8d33812574cef92dae759e11708a022fd48e540a
Jesse B. Hannah <jesse@jbhannah.net>
2021-03-24 12:40:03 -0700

    Remove local Pipenv, it'll be installed globally

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 python3 -m venv --system-site-packages .venv
 . .venv/bin/activate
-pip install pipenv
+pip install pipenv pip-autoremove
 pipenv install
 pipenv run ansible-playbook ansible/dotfiles.yml
+pip-autoremove pipenv pip-autoremove -y
+deactivate
