1635e6e00f3f823493de7760901cff59be6414c8
Jesse B. Hannah <jesse@jbhannah.net>
2021-05-17 15:48:42 -0700

    macOS-specific config

---
+++ dotfiles/.config/fish/conf.d/##os.Darwin/postgres-app.fish
@@ -0,0 +1,2 @@
+test -d /Applications/Postgres.app
+and fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,3 +2,8 @@ status is-interactive
 and set -g fish_greeting
 
 abbr -ag l ls -alh
+
+set -q VISUAL
+or set -gx VISUAL vim
+
+set -gx EDITOR $VISUAL
