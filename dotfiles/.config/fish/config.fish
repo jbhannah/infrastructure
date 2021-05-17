b0367b59fa8fa92d35563145b4511d973345c198
Jesse B. Hannah <jesse@jbhannah.net>
2021-05-17 15:48:27 -0700

    ls and exa helpers

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,2 +1,4 @@
 status is-interactive
 and set -g fish_greeting
+
+abbr -ag l ls -alh
---
+++ dotfiles/.config/fish/functions/ls.fish
@@ -0,0 +1,3 @@
+status is-interactive
+and command -sq exa
+and alias ls="exa --links --git --group --group-directories-first"
