b855987c6b8fd70f8861d688200907eac639af65
Jesse B. Hannah <jesse@jbhannah.net>
2021-05-18 11:54:14 -0700

    Fish vi key bindings

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,7 +1,14 @@
-status is-interactive
-and set -g fish_greeting
+if status is-interactive
+    set -g fish_greeting
 
-abbr -ag l ls -alh
+    set -g fish_key_bindings fish_vi_key_bindings
+    set -g fish_vi_force_cursor 1
+    set -g fish_cursor_default block
+    set -g fish_cursor_insert line
+    set -g fish_cursor_replace_one underscore
+
+    abbr -ag l ls -alh
+end
 
 set -q VISUAL
 or set -gx VISUAL vim
