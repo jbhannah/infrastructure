e055d64a97a3abefa889a4c685d87adcd16abb81
Jesse B. Hannah <jesse@jbhannah.net>
2021-08-30 13:34:42 -0700

    Allow separate kubectl contexts per session
    https://jackma.com/2019/11/23/one-kubectl-context-per-shell-session/

--- ansible/roles/kubectl/files/fish/conf.d/kubectl.fish
+++ ansible/roles/kubectl/files/fish/conf.d/kubectl.fish
@@ -1,2 +1,10 @@
 test -d $HOME/.krew/bin
 and fish_add_path $HOME/.krew/bin
+
+set -l local_kubeconfig (mktemp -t "kubectl")
+set -gx KUBECONFIG "$local_kubeconfig:$HOME/.kube/config"
+echo "\
+apiVersion: v1
+kind: Config
+current-context: \"\"
+" >$local_kubeconfig
