1fb46cd1d7edb8ba37e3aef388b2694e6f7e928a
Jesse B. Hannah <jesse@jbhannah.net>
2022-02-16 13:14:47 -0800

    Add more Gmail DNS records

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,6 +6,36 @@ resource "cloudflare_zone_dnssec" "hannahs_family" {
   zone_id = cloudflare_zone.hannahs_family.id
 }
 
+resource "cloudflare_record" "MX_hannahs_family" {
+  for_each = {
+    "aspmx.l.google.com"      = 1
+    "alt1.aspmx.l.google.com" = 5
+    "alt2.aspmx.l.google.com" = 5
+    "aspmx2.googlemail.com"   = 10
+    "aspmx3.googlemail.com"   = 10
+  }
+
+  zone_id  = cloudflare_zone.hannahs_family.id
+  name     = "hannahs.family"
+  type     = "MX"
+  priority = each.value
+  value    = each.key
+}
+
+resource "cloudflare_record" "TXT_hannahs_family" {
+  zone_id = cloudflare_zone.hannahs_family.id
+  name    = "hannahs.family"
+  type    = "TXT"
+  value   = "v=spf1 a include:_spf.google.com ~all"
+}
+
+resource "cloudflare_record" "TXT__dmarc_hannahs_family" {
+  zone_id = cloudflare_zone.hannahs_family.id
+  name    = "_dmarc"
+  type    = "TXT"
+  value   = "v=DMARC1; p=quarantine; rua=mailto:admin@hannahs.family"
+}
+
 resource "cloudflare_zone" "jbhannah_net" {
   zone = "jbhannah.net"
 }
@@ -62,6 +92,13 @@ resource "cloudflare_record" "TXT_jbhannah_net" {
   name    = "jbhannah.net"
   type    = "TXT"
   value   = each.value
+}
+
+resource "cloudflare_record" "TXT__dmarc_jbhannah_net" {
+  zone_id = cloudflare_zone.jbhannah_net.id
+  name    = "_dmarc"
+  type    = "TXT"
+  value   = "v=DMARC1; p=quarantine; rua=mailto:admin@jbhannah.net"
 }
 
 resource "cloudflare_record" "TXT__keybase_jbhannah_net" {
